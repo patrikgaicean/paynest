d73b7b6a7d76d57090999bcca3469d0773a30dad
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-10-18 00:11:15 +0300

    add class-validator and class-transformer packages, use global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   dotenv.config();
@@ -9,6 +10,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   const port = config.get('PORT');
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
 }
