c3d460b34daae39db810005c747287896b35ada5
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-10-18 01:53:58 +0300

    add validation for create-transaction

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,5 +1,44 @@
+import { IsEnum, IsNotEmpty, IsNumber, Min, ValidateIf, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, registerDecorator } from "class-validator";
+import { TransactionType } from "./interfaces";
+
+@ValidatorConstraint({ name: "maxAmount", async: false })
+export class MaxAmountValidator implements ValidatorConstraintInterface {
+  validate(value: number, args: ValidationArguments) {
+    const { object } = args;
+
+    if ((object as CreateTransactionDto).type === TransactionType.transfer) {
+      return value <= 5000;
+    } else {
+      return value <= 100_000;
+    }
+  }
+}
+
+export function MaxAmount(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: MaxAmountValidator,
+    });
+  };
+}
+
 export class CreateTransactionDto {
-  type: string; // top-up / transfer
-  accountId?: string; // if transfer
-  amount: number; // [$1, $5000]
+  @IsEnum(TransactionType, {
+    message: "Invalid transaction type. Must be 'topup' or 'transfer'.",
+  })
+  @IsNotEmpty()
+  type: TransactionType;
+
+  @ValidateIf((t: CreateTransactionDto) => t.type === TransactionType.transfer )
+  @IsNumber()
+  @Min(1)
+  accountId?: number;
+
+  @Min(1)
+  @MaxAmount() // 5000 for top-ups, 100_000 for transfers
+  amount: number;
 }
---
+++ src/transactions/dto/interfaces.ts
@@ -0,0 +1,10 @@
+export enum TransactionType {
+  topup = 'topup',
+  transfer = 'transfer',
+}
+
+export enum TransactionStatus {
+  pending = 'pending',
+  succeeded = 'succeeded',
+  failed = 'failed',
+}
