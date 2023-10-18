51f399bd8f96754b121c4afc706d4335a57cd847
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-10-18 21:53:00 +0300

    update maxAmount validator to show error correctly

--- src/validators/maxAmount.validator.ts
+++ src/validators/maxAmount.validator.ts
@@ -1,16 +1,26 @@
 import { ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, registerDecorator } from "class-validator";
-import { TransactionType } from "src/transactions/dto/interfaces";
+import { TransactionType } from "../transactions/dto/interfaces";
 
 @ValidatorConstraint({ name: "maxAmount", async: false })
 export class MaxAmountValidator implements ValidatorConstraintInterface {
   validate(value: number, args: ValidationArguments) {
     const { object } = args;
 
-    if ((object as { type: TransactionType}).type === TransactionType.transfer) {
-      return value <= 5000;
+    if ((object as { type: TransactionType}).type === TransactionType.topup) {
+      if (value > 5000) {
+        return false;
+      }
     } else {
-      return value <= 100_000;
+      if (value > 100000) {
+        return false;
+      }
     }
+
+    return true;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return 'Transfer amount exceeds permitted limit.';
   }
 }
 
