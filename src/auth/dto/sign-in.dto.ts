69db4dcda588ec2fae71ee8b0a7972831cb66c57
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-10-19 00:29:01 +0300

    update sign-in docs and validation

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,23 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsStrongPassword } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class SignInDto {
   @ApiProperty({
-    description: "A valid email address.",
+    description: "Email address you registered with.",
     example: "someone@gmail.com",
   })
   @IsEmail()
   email: string;
 
   @ApiProperty({
-    description: "Password - must contain at least 3 digits, 3 lowercase characters, 2 uppercase characters, one symbol.",
+    description: "Password associated to your accont.",
     example: "this1$AdecentPA55",
   })
-  @IsStrongPassword({
-    minNumbers: 3,
-    minLowercase: 3,
-    minUppercase: 2,
-    minSymbols: 1,
-  })
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
