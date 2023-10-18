6480b39e42e048004bf7918325ef4a6b590a50ef
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-10-18 18:50:03 +0300

    update transactions controller docs

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -11,27 +11,32 @@ import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { DeleteTransactionDto } from './dto/delete-transaction.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all existing transactions.' })
   getAll() {
     return this.transactionsService.getAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get one transaction by id.' })
   getOne(@Param('id') id: string) {
     return this.transactionsService.getOne(+id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new transaction.' })
   create(@Body() createTransactionDto: CreateTransactionDto) {
     return this.transactionsService.create(createTransactionDto);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update an existing transaction.' })
   update(
     @Param('id') id: string,
     @Body() updateTransactionDto: UpdateTransactionDto,
@@ -40,6 +45,7 @@ export class TransactionsController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete an existing transaction.' })
   delete(
     @Param('id') id: string,
     @Body() deleteTransactionDto: DeleteTransactionDto,
