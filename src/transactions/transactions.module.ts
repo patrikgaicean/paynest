f1e9a907349f09ee0ba582b630b4284085edddf1
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-10-18 20:17:38 +0300

    update transactions module

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Transaction]),
+    UsersModule,
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
+  exports: [TypeOrmModule],
 })
 export class TransactionsModule {}
